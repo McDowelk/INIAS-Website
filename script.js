a6bc3e6e90aa198a5660a2b6c81fbe76c2e2407b
McDowelk <kristof@obam.dev>
2025-07-13 12:48:32 +0200

    Update script.js
    - Added calendar

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// script.js
-function sayHi() {
-  alert("Hoi Kristof!");
-}
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'title',
+      right: 'prev,next'
+    },
+    events: [
+      { title: 'Ancienne Belgique, Brussel', start: '2025-07-20' },
+      { title: 'Lowlands, Nederland',       start: '2025-08-15' }
+    ]
+  });
+  calendar.render();
+});
